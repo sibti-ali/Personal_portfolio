74532ab4f95ff6742cd951032106b52df94d0b79
SibtainAli <alimohammedjoo@gmail.com>
2025-10-18 18:42:24 +0100

    Removed unused imports

--- frontend/src/Pages/NotFound.tsx
+++ frontend/src/Pages/NotFound.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Home, ArrowRight } from 'lucide-react';
 
 export default function NotFound() {
